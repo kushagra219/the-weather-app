be80e6578212981867fc46a0296d2de87588cf69
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-02-16 13:07:45 +0530

    error

---
+++ weather/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+# Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e73d2409d9763572716a454cc2b36d0e'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/weather.html', context)
